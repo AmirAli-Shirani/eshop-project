58d6e6d136aa209a0a7b0c8ab0c45746b48f7939
AmirAli <amiralisherani10@gmail.com>
2024-09-28 19:28:43 +0330

    second commit

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#^5+h4)=y(=pa2q8po&$tn^kqo5$z=l$j25g-def4c=^c=4rf!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [h for h in os.getenv('ALLOWED_HOSTS', '').split(",") if h]
 
 # Application definition
 
