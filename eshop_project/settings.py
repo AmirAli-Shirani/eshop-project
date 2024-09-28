bf4a2b918a3468dbe7735b69d4a8e9c8170b3dd5
AmirAli <147398793+AmirAli-Shirani@users.noreply.github.com>
2024-09-28 20:24:58 +0330

    Update settings.py

--- eshop_project/settings.py
+++ eshop_project/settings.py
@@ -134,11 +134,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'amiralisherani10@gmail.com'
-EMAIL_HOST_PASSWORD = 'chvwmcfrqocllwnc'
-EMAIL_PORT = 587
+
 
 
